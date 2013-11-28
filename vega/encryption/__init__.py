2ccff5727bce66524cfd8db504fd0a8e375909e0
Hugo Herter <github.dev@hugoh.org>
2013-11-28 15:45:27 +0100

    Created submodules and class skeletons

--- vega/encryption/__init__.py
+++ vega/encryption/__init__.py
@@ -15,6 +15,3 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
-
-__author__ = 'Hugo Herter http://hugoherter.com'
-__version__ = 0.02
--- vega/encryption/test.py
+++ vega/encryption/test.py
@@ -16,5 +16,10 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-__author__ = 'Hugo Herter http://hugoherter.com'
-__version__ = 0.02
+
+class DummyEncryption:
+    pass
+
+
+class DummyDecryption:
+    pass
